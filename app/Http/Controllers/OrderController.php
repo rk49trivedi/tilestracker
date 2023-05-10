b1c2ee0aaeeef2290053dcc08824df3ac4d49ff3
rk49trivedi <49roshan@gmail.com>
2023-05-10 17:52:22 +0530

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,18 +9,26 @@ use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    
+
+    public function rezorPayReturn(Request $request){
+
+        echo '<pre>';
+        print_r($request->all());
+        exit;
+
+    }
+
     public function showCheckout(){
         return view('checkout');
     }
     public function viewOrders(){
 
-        $allOrders = DB::table('orders')->join('price','orders.plan_id','=','price.id')->select('price.name','orders.*')->where('user_id',session()->get('unlocker_user')[0])->get();
+        $allOrders = DB::table('orders')->join('price','orders.plan_id','=','price.id')->select('price.name','orders.*')->where('user_id',session()->get('unlocker_user')[0])->orderBy('order_id','desc')->paginate(10);
         return view('orders',compact('allOrders'));
     }
 
     public function processtoPay(Request $request){
-        
+
         $api_key = 'rzp_test_y1JxbNUs1Aw0r9';
         $api_secret = 'DXpokIEhKme19AC05990F2S5';
 
@@ -30,7 +38,7 @@ class OrderController extends Controller
 
 
         $order = $api->order->create([
-            'amount' => $request->amount * 100, 
+            'amount' => $request->amount * 100,
             'currency' => 'INR',
             'payment_capture' => 1,
             'notes' => [
@@ -39,7 +47,7 @@ class OrderController extends Controller
             ],
         ]);
 
-        
+
         // create order
 
         $insertData = DB::table('orders')->insert(['user_id'=>session()->get('unlocker_user')[0],'plan_id'=>$request->plan_id,'price'=>$request->amount,'transaction_id'=>$order->id,'order_json'=>json_encode($order)]);
@@ -54,16 +62,55 @@ class OrderController extends Controller
                 'razorpay_key'=>$api_key
             ]);
         }
-        
+
 
     }
 
     public function Paymentprocess(Request $request){
 
+        $input = $request->all();
+
         $api_key = 'rzp_test_y1JxbNUs1Aw0r9';
         $api_secret = 'DXpokIEhKme19AC05990F2S5';
 
         $api = new Api($api_key, $api_secret);
+
+        $payment = $api->payment->fetch($request->input('razorpay_payment_id'));
+
+        $order_id = $payment->notes->order_id;
+
+        if(count($input) && !empty($request->input('razorpay_payment_id'))) {
+            try {
+                $response = $api->payment->fetch($input['razorpay_payment_id'])->capture(array('amount' => $payment->amount));
+
+                DB::table('orders')->where('transaction_id',$order_id)->update([
+                    'r_payment_id' => $response['id'],
+                    'method' => $response['method'],
+                    'currency' => $response['currency'],
+                    'user_email' => $response['email'],
+                    'status' => 'completed',
+                    'order_json' => json_encode((array)$response)
+                ]);
+
+                return redirect('orders')->with('success','Payment successfully completed');
+
+            } catch(Exceptio $e) {
+
+                DB::table('orders')->where('transaction_id',$order_id)->update([
+                    'status' => 'failed'
+                ]);
+
+                return redirect('orders')->with('error',$e->getMessage());
+            }
+        }else{
+            DB::table('orders')->where('transaction_id',$order_id)->update([
+                'status' => 'failed'
+            ]);
+            return redirect('orders')->with('error','Invalid request found');
+        }
+
+
+        exit;
 
         $payment = $api->payment->fetch($request->input('razorpay_payment_id'));
 
@@ -75,6 +122,10 @@ class OrderController extends Controller
         $signature = hash_hmac('sha256', $string, $api_secret);
         $signature = base64_encode($signature);
 
+
+
+
+
         try {
 
             $attributes = array(
@@ -82,13 +133,19 @@ class OrderController extends Controller
                 'razorpay_payment_id' => $payment_id,
                 'razorpay_signature' => $signature
             );
-        
+
             $api->utility->verifyPaymentSignature($attributes);
-            
+
+            echo '<pre>';
+            print_r($request->all());
+            exit;
+
             return redirect('orders')->with('success','Payment successfully completed');
 
         } catch (\Exception $e) {
-            return redirect('orders')->with('error',$e->getMessage());
+            echo $e->getMessage();
+            exit;
+            //return redirect('orders')->with('error',$e->getMessage());
         }
 
     }
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,84 +1,101 @@
 @extends('layout.mainlayout')
-@section('content')		
+@section('content')
 
-<!-- Banner Section -->
-<section class="page-banner">
-    <div class="image-layer" style="background-image:url({{asset('images/background/banner-image-2.jpg')}});"></div>
-    <div class="banner-bottom-pattern"></div>
+    <!-- Banner Section -->
+    <section class="page-banner">
+        <div class="image-layer" style="background-image:url({{asset('images/background/banner-image-2.jpg')}});"></div>
+        <div class="banner-bottom-pattern"></div>
 
-    <div class="banner-inner">
-        <div class="auto-container">
-            <div class="inner-container clearfix">
-                <h1>My Orders</h1>
-                <div class="page-nav">
-                    <ul class="bread-crumb clearfix">
-                        <li><a href="{{url('/')}}">Home</a></li>
-                        <li class="active">My Orders</li>
-                    </ul>
+        <div class="banner-inner">
+            <div class="auto-container">
+                <div class="inner-container clearfix">
+                    <h1>My Orders</h1>
+                    <div class="page-nav">
+                        <ul class="bread-crumb clearfix">
+                            <li><a href="{{url('/')}}">Home</a></li>
+                            <li class="active">My Orders</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<!--End Banner Section -->
+    </section>
 
-<!--Offers Section-->
-<section class="packages-section">
-    <div class="auto-container">
-        <div class="title-box">
-            <h2>My Orders</h2>
-        </div>
-        <div class="container-fluid">
-            <div class="container">
-                <div class="row">
+    <style>
+        svg.w-5.h-5 {
+            width: 20px;
+        }
+        p.text-sm.text-gray-700.leading-5{margin:20px 0px;}
+    </style>
+    <!--End Banner Section -->
 
-                    @if(session()->has('success'))
-                        <div class="form-col col-md-12 col-sm-12 alert alert-success">
-                            {{session()->get('success')}}
-                        </div>
-                    @endif
+    <!--Offers Section-->
+    <section class="packages-section">
+        <div class="auto-container">
+            <div class="title-box">
+                <h2>My Orders</h2>
+            </div>
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="row">
 
-                    @if(session()->has('error'))
-                        <div class="form-col col-md-12 col-sm-12 alert alert-danger">
-                            {{session()->get('error')}}
-                        </div>
-                    @endif
+                        @if(session()->has('success'))
+                            <div class="form-col col-md-12 col-sm-12 alert alert-success">
+                                {{session()->get('success')}}
+                            </div>
+                        @endif
 
-                    <div class="col-md-12">
-                        <table class="table">
-                            <thead>
+                        @if(session()->has('error'))
+                            <div class="form-col col-md-12 col-sm-12 alert alert-danger">
+                                {{session()->get('error')}}
+                            </div>
+                        @endif
+
+                        <div class="col-md-12">
+                            <table class="table">
+                                <thead>
                                 <th>OrderID</th>
                                 <th>Transaction ID</th>
                                 <th>Status</th>
                                 <th>Plan</th>
-                            </thead>
-                            
-                        @foreach($allOrders as $priceDetail)
+                                </thead>
 
-                        <tbody>
-                            <tr>
-                                <td>#{{$priceDetail->order_id}}</td>
-                                <td>{{$priceDetail->transaction_id}}</td>
-                                <td>
-                                    @if($priceDetail->status == 'pending')
-                                    <span class="badge badge-danger">{{$priceDetail->status}}</span>
-                                    @else
-                                    <span class="badge badge-success">{{$priceDetail->status}}</span>
+                                @foreach($allOrders as $priceDetail)
+
+                                    <tbody>
+                                    <tr>
+                                        <td>#{{$priceDetail->order_id}}</td>
+                                        <td>{{$priceDetail->transaction_id}}</td>
+                                        <td>
+                                            @if($priceDetail->status == 'pending')
+                                                <span class="badge badge-danger">{{$priceDetail->status}}</span>
+                                            @else
+                                                <span class="badge badge-success">{{$priceDetail->status}}</span>
+                                            @endif
+                                        </td>
+                                        <td>{{$priceDetail->name}}</td>
+                                    </tr>
+                                    </tbody>
+
+                                @endforeach
+
+                                <tfoot>
+                                <tr>
+                                    @if ($allOrders->links()->paginator->hasPages())
+                                        <td class="pagination">
+                                            {{ $allOrders->links() }}
+                                        </td>
                                     @endif
-                                </td>
-                                <td>{{$priceDetail->name}}</td>
-                            </tr>
-                        </tbody>
-                    
-                        @endforeach
+                                </tr>
+                                </tfoot>
 
-                        </table>
+                            </table>
+                        </div>
+
+
                     </div>
-                    
-                    
                 </div>
             </div>
-        </div>
-</section>
+    </section>
 
 @endsection
